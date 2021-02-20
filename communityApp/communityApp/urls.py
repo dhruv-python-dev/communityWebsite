4a4f7235dfe3448df0fe653ab855967e8a87eae5
DabhiMeet <72125764+DabhiMeet@users.noreply.github.com>
2021-02-20 20:01:09 +0530

    Comment url added

--- communityApp/communityApp/urls.py
+++ communityApp/communityApp/urls.py
@@ -32,4 +32,7 @@ urlpatterns = [
 
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
+    
+    path('Comment/', blog_views.Commentlist.as_view()),
+    
 ]
