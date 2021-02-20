07edb32688c411ea85d601c9a9ad7babe0939922
DabhiMeet <72125764+DabhiMeet@users.noreply.github.com>
2021-02-20 19:07:54 +0530

    Comment serializer added

--- communityApp/blog/serializers.py
+++ communityApp/blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Blog
+from .models import Blog, Comment
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,3 +18,22 @@ class BlogSerializer(serializers.ModelSerializer):
         instance.slug = validated_data.get('slug', instance.slug)
         instance.user = validated_data.get('user', instance.user)
         return instance
+
+    
+    class CommentSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Blog
+        fields = '__all__'
+
+        def create(self,validated_data):
+            return Comment(**validated_data)
+
+        def update(self, instance, validated_data):
+            instance.date = validated_data.get('date',instance.date)
+            instance.body = validated_data.get('body',instance.body)
+            instance.likes =validated_data.get('likes',instance.likes)
+            instance.user = validated_data.get('user',instance.user)
+            instance.blog = validated_data.get('blog',instance.blog)
+            return instance
+    
+    
