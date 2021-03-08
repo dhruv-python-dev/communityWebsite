7957c6ddf24d6aa10a3af7faf14647c2210d0492
Dhruv Patel - Fero <79076564+dhruvpatel-fero@users.noreply.github.com>
2021-03-08 10:44:50 +0530

    Update serializers.py

--- communityApp/blog/serializers.py
+++ communityApp/blog/serializers.py
@@ -26,7 +26,7 @@ class CustomSerializer(serializers.ModelSerializer):
     def get_field_names(self, declared_fields, info):
         expanded_fields = super(CustomSerializer, self).get_field_names(declared_fields, info)
 
-        if getattr(self.Meta, 'extra_fields', None):
+        if getattr(self.Meta, 'related_fields', None):
             return expanded_fields + self.Meta.related_fields
         else:
             return expanded_fields
